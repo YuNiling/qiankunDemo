441ea58e356d1726a8a6dd48ce99b19a5e0003be
YN Wang <ynwang@YNdeMacBook-Air.local>
2025-04-24 11:22:35 +0800

    封装axios实现缓存、防抖、重试功能，并实现token校验和拦截

---
+++ subApp/vue3-sub-app/src/views/Vant4Demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <van-button type="primary">按钮</van-button>
+  </div>
+</template>
+
+<script setup>
+import { Button as VantButton } from 'vant';
+
+</script>
+
+<style lang="less" scoped>
+.van-button {
+  background-color: @mobile-primary-color;
+}
+</style>
--- subApp/vue3-sub-app/vite.config.js
+++ subApp/vue3-sub-app/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import qiankun from 'vite-plugin-qiankun';
 import path from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 const port = 8002;
@@ -13,6 +18,19 @@ export default defineConfig({
     vue(),
     qiankun('vue3-sub-app', { // 微应用名字，与主应用注册的微应用名字保持一致
       useDevMode // true：不使用热更新插件，false：使用热更新，但无法作为子应用加载
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver(), VantResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(), VantResolver()]
+    }),
+    viteCompression({
+      verbose: true,  // 是否在控制台输出压缩日志
+      disable: false, // 是否禁用
+      threshold: 1024 * 1024 * 3, // 压缩大小限制 超过 3MB 才压缩
+      algorithm: 'gzip', // 压缩算法
+      ext: '.gz', // 压缩文件后缀
     })
   ],
   server: {
@@ -31,18 +49,49 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       less: {
-        additionalData: `@import "${path.resolve(__dirname, 'src/assets/variables.less')}";`,
+        additionalData: `
+          @import "${path.resolve(__dirname, 'src/assets/variables.less')}";
+          @import "${path.resolve(__dirname, 'src/assets/variables-mobile.less')}";
+        `,
         javascriptEnabled: true
+      },
+      sass: {
+        additionalData: `@use "${path.resolve(__dirname, 'src/assets/variables-pc.sass')}" as *;`,
       }
     }
   },
   build: {
-    target: 'esnext',
-    assetsInlineLimit: 0,
-    lib: {
-      entry: './src/main.js',
-      name: 'vue3-sub-app',
-      formats: ['umd']
+    minify: 'esbuild',
+    // terserOptions: {
+    //   compress: {
+    //     drop_console: true, // 移除console
+    //     pure_funcs: ['console.log'] // 移除指定函数
+    //   }
+    // },
+    // target: 'esnext',
+    assetsInlineLimit: 1024 * 1024 * 3, // 小于 3MB 的图片会被转为 base64
+    // lib: {
+    //   entry: './src/main.js',
+    //   name: 'vue3-sub-app',
+    //   formats: ['umd']
+    // },
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]',
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        entryFileNames: 'assets/js/[name].[hash].js',
+        manualChunks(src) {
+          if (src.includes('node_modules')) {
+            if (src.includes('element-plus')) {
+              return 'element-plus';
+            }
+            if (src.includes('vant')) {
+              return 'vant';
+            }
+            return 'vendor';
+          }
+        }
+      }
     }
   }
 });
